1b7791fb0320b7cc8864490ba748cf795c3cf72d
AmberZxxiv <amberzxxiv@gmail.com>
2025-11-20 11:16:44 +0100

    Salas
    prefabs

---
+++ SmollTrops/Assets/Scripts/Room_Manager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Room_Manager : MonoBehaviour
+{
+    public GameObject[] room1X;
+    public GameObject[] room0X;
+    public GameObject[] room1Z;
+    public GameObject[] room0Z;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SmollTrops/Assets/Scripts/Room_Spawner.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Room_Spawner : MonoBehaviour
+{
+    public int doorDirection;
+    // 1 = door en 1X
+    // 2 = door en 0X
+    // 3 = door en 1Z
+    // 4 = door en 0Z
+
+    private
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doorDirection == 1) //busca sala 1X
+        {
+
+        }
+        if (doorDirection == 2) //busca sala 0X
+        {
+
+        }
+        if (doorDirection == 3) //busca sala 1Z
+        {
+
+        }
+        if (doorDirection == 4) //busca sala 0X
+        {
+
+        }
+    }
+}
