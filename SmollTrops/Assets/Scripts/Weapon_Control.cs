41a724dfcc244b10044aeeb5c1ce829828519d38
AmberZxxiv <amberzxxiv@gmail.com>
2025-12-03 19:47:31 +0100

    pre weapon

---
+++ SmollTrops/Assets/Scripts/Weapon_Control.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Weapon_Control : MonoBehaviour
+{
+    public WeaponType weapon;
+    public enum WeaponType
+    {
+        Kick,
+        Punch,
+        Gun,
+        Magic
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
